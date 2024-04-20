0fbe6e0670f1b2db4bd6a27671b6613fdf18ea5a
perfectbogus <war_army_10@hotmail.com>
2024-04-20 14:11:52 -0600

    Section 8: Order Management Kafka Stream Application - A real time use case

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -4,16 +4,12 @@ import com.learnkafkastreams.domain.Greeting;
 import com.learnkafkastreams.sedes.SerdesFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @Slf4j
 public class GreetingsTopology {

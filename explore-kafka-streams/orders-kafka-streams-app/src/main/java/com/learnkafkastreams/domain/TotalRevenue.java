a5b5f3791f5afd064f0aa9bb0ba3e5895be7a0dd
perfectbogus <war_army_10@hotmail.com>
2024-04-21 11:48:42 -0600

    Section 15: Re-Keying Kafka Records for Stateful Operations

--- explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/domain/TotalRevenue.java
+++ explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/domain/TotalRevenue.java
@@ -3,6 +3,17 @@ package com.learnkafkastreams.domain;
 import java.math.BigDecimal;
 
 public record TotalRevenue(String locationId,
-                           Integer runnuingOrderCount,
+                           Integer runningOrderCount,
                            BigDecimal runningRevenue) {
+
+    public TotalRevenue(){
+        this("", 0, BigDecimal.ZERO);
+    }
+
+    public TotalRevenue updateRunningRevenue(String key, Order order) {
+        var newOrdersCount = runningOrderCount + 1;
+        var newRevenue = runningRevenue.add(order.finalAmount());
+        return new TotalRevenue(key, newOrdersCount, newRevenue);
+    }
+
 }
--- explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
+++ explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
@@ -3,12 +3,16 @@ package com.learnkafkastreams.topology;
 import com.learnkafkastreams.domain.Order;
 import com.learnkafkastreams.domain.OrderType;
 import com.learnkafkastreams.domain.Revenue;
+import com.learnkafkastreams.domain.TotalRevenue;
 import com.learnkafkastreams.sedes.SerdesFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 @Slf4j
 public class OrdersTopology {
@@ -18,8 +22,11 @@ public class OrdersTopology {
     public static final String RESTAURANT_ORDERS = "restaurant_orders";
     public static final String GENERAL_ORDERS = "general_orders";
 
+    public static final String GENERAL_ORDERS_COUNT = "general_orders_count";
+    public static final String RESTAURANT_ORDERS_COUNT = "restaurant_orders_count";
 
-
+    private static final String GENERAL_ORDERS_REVENUE = "general_orders_revenue";
+    private static final String RESTAURANT_ORDERS_REVENUE = "restaurant_orders_revenue";
 
 
     public static Topology buildTopology(){
@@ -29,7 +36,7 @@ public class OrdersTopology {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
 
         var ordersStream = streamsBuilder.stream(ORDERS,
-                    Consumed.with(Serdes.String(), SerdesFactory.orderSerde())
+                    Consumed.with(Serdes.String(), SerdesFactory.orderSerdes())
                 );
 
         ordersStream.print(Printed.<String, Order >toSysOut().withLabel("orders"));
@@ -41,10 +48,14 @@ public class OrdersTopology {
                             generalOrderStream
                                     .print(Printed.<String, Order>toSysOut().withLabel("generalStream"));
 
-                            generalOrderStream
-                                    .mapValues((readOnlyKey, value) -> revenueMapper.apply(value))
-                                    .to(GENERAL_ORDERS,
-                                            Produced.with(Serdes.String(), SerdesFactory.revenueSerde()));
+//                            generalOrderStream
+//                                    .mapValues((readOnlyKey, value) -> revenueMapper.apply(value))
+//                                    .to(GENERAL_ORDERS,
+//                                            Produced.with(Serdes.String(), SerdesFactory.revenueSerde()));
+
+                            aggregateOrdersByCount(generalOrderStream, GENERAL_ORDERS_COUNT);
+                            aggregateOrdersByRevenue(generalOrderStream, GENERAL_ORDERS_REVENUE);
+
                         })
                 )
                 .branch(restaurantPredicate,
@@ -52,13 +63,50 @@ public class OrdersTopology {
                             restaurantOrderStream
                                     .print(Printed.<String, Order>toSysOut().withLabel("restaurantStream"));
 
-                            restaurantOrderStream
-                                    .mapValues((readOnlyKey, value) -> revenueMapper.apply(value))
-                                    .to(RESTAURANT_ORDERS,
-                                            Produced.with(Serdes.String(), SerdesFactory.revenueSerde()));
+//                            restaurantOrderStream
+//                                    .mapValues((readOnlyKey, value) -> revenueMapper.apply(value))
+//                                    .to(RESTAURANT_ORDERS,
+//                                            Produced.with(Serdes.String(), SerdesFactory.revenueSerde()));
+                            aggregateOrdersByCount(restaurantOrderStream, RESTAURANT_ORDERS_COUNT);
+                            aggregateOrdersByRevenue(restaurantOrderStream, RESTAURANT_ORDERS_REVENUE);
                         })
                 );
 
         return streamsBuilder.build();
+    }
+
+    private static void aggregateOrdersByRevenue(KStream<String, Order> generalOrderStream, String storedName) {
+        //Initializer
+        Initializer<TotalRevenue> totalRevenueInitializer = TotalRevenue::new;
+        //aggregator
+        Aggregator<String, Order, TotalRevenue> aggregator = (key, value, aggregate) ->
+                aggregate.updateRunningRevenue(key, value);
+
+        var revenueKTable = generalOrderStream
+                .map((key, value) -> KeyValue.pair(value.locationId(), value))
+                .groupByKey(Grouped.with(Serdes.String(), SerdesFactory.orderSerdes()))
+                .aggregate(
+                        totalRevenueInitializer,
+                        aggregator,
+                        Materialized.<String, TotalRevenue, KeyValueStore<Bytes, byte[]>>as(storedName)
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(SerdesFactory.totalRevenueSerdes())
+
+                );
+        revenueKTable
+                .toStream()
+                .print(Printed.<String, TotalRevenue>toSysOut().withLabel(storedName));
+
+    }
+
+    private static void aggregateOrdersByCount(KStream<String, Order> generalOrderStream, String storedName) {
+        var ordersCountPerStore = generalOrderStream
+                .map((key, value) -> KeyValue.pair(value.locationId(), value))
+                .groupByKey(Grouped.with(Serdes.String(), SerdesFactory.orderSerdes()))
+                .count(Named.as(storedName), Materialized.as(storedName));
+
+        ordersCountPerStore
+                .toStream()
+                .print(Printed.<String, Long>toSysOut().withLabel(storedName));
     }
 }

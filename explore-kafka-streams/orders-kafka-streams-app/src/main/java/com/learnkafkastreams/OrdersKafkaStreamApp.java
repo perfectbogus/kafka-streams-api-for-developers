a708d34b4e47b168969483fd9a70ab904eb0af56
perfectbogus <war_army_10@hotmail.com>
2024-04-24 18:19:07 -0600

    Section 19: Windowing in Order Management Application - A Real Time Use Case

--- explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/OrdersKafkaStreamApp.java
+++ explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/OrdersKafkaStreamApp.java
@@ -27,7 +27,7 @@ public class OrdersKafkaStreamApp {
         config.put(StreamsConfig.APPLICATION_ID_CONFIG, "orders-app"); // consumer group
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest"); // read only the new messages
-        config.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, OrderTimeStampExtractor.class);
+        //config.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, OrderTimeStampExtractor.class);
 
         createTopics(config, List.of(OrdersTopology.STORES, OrdersTopology.ORDERS, OrdersTopology.GENERAL_ORDERS, OrdersTopology.RESTAURANT_ORDERS));
 
--- explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
+++ explore-kafka-streams/orders-kafka-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
@@ -2,7 +2,9 @@ package com.learnkafkastreams.topology;
 
 import com.learnkafkastreams.domain.*;
 import com.learnkafkastreams.sedes.SerdesFactory;
+import com.learnkafkastreams.util.OrderTimeStampExtractor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
@@ -11,6 +13,7 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.streams.kstream.internals.TimeWindow;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.WindowStore;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -44,6 +47,7 @@ public class OrdersTopology {
 
         var ordersStream = streamsBuilder.stream(ORDERS,
                     Consumed.with(Serdes.String(), SerdesFactory.orderSerdes())
+                            .withTimestampExtractor(new OrderTimeStampExtractor())
                 )
                 .selectKey((key, value) -> value.locationId());
 
@@ -87,6 +91,7 @@ public class OrdersTopology {
 //                            aggregateOrdersByCount(restaurantOrderStream, RESTAURANT_ORDERS_COUNT, storesTable);
                             aggregateOrdersCountByTimeWindows(restaurantOrderStream, RESTAURANT_ORDERS_COUNT_WINDOWS, storesTable);
 //                            aggregateOrdersByRevenue(restaurantOrderStream, RESTAURANT_ORDERS_REVENUE, storesTable);
+                            aggregateOrdersRevenueByTimeWindows(restaurantOrderStream, RESTAURANT_ORDERS_REVENUE_WINDOWS, storesTable);
                         })
                 );
 
@@ -94,6 +99,12 @@ public class OrdersTopology {
     }
 
     private static void aggregateOrdersRevenueByTimeWindows(KStream<String, Order> generalOrderStream, String storedName, KTable<String, Store> storesTable) {
+
+        Duration windowSize = Duration.ofSeconds(15);
+        TimeWindows timeWindows = TimeWindows.ofSizeWithNoGrace(windowSize);
+
+
+
         //Initializer
         Initializer<TotalRevenue> totalRevenueInitializer = TotalRevenue::new;
         //aggregator
@@ -103,10 +114,11 @@ public class OrdersTopology {
         var revenueKTable = generalOrderStream
                 .map((key, value) -> KeyValue.pair(value.locationId(), value))
                 .groupByKey(Grouped.with(Serdes.String(), SerdesFactory.orderSerdes()))
+                .windowedBy(timeWindows)
                 .aggregate(
                         totalRevenueInitializer,
                         aggregator,
-                        Materialized.<String, TotalRevenue, KeyValueStore<Bytes, byte[]>>as(storedName)
+                        Materialized.<String, TotalRevenue, WindowStore<Bytes, byte[]>>as(storedName)
                                 .withKeySerde(Serdes.String())
                                 .withValueSerde(SerdesFactory.totalRevenueSerdes())
 
@@ -114,18 +126,30 @@ public class OrdersTopology {
 
         revenueKTable
                 .toStream()
-                .print(Printed.<>toSysOut().withLabel(storedName));
+                .peek((key, value) -> {
+                    log.info("StoreName: {}, key: {}, value: {}", storedName, key, value);
+                    printLocalDateTimes(key, value);
+                })
+                .print(Printed.<Windowed<String>, TotalRevenue>toSysOut().withLabel(storedName + "-bystore"));
 
         //KTable-KTable Join
-//        ValueJoiner<TotalRevenue, Store, TotalRevenueWithAddress> valueJoiner = TotalRevenueWithAddress::new;
-//
-//        var revenueWithStoreTable = revenueKTable
-//                .join(
-//                        storesTable,
-//                        valueJoiner
-//                );
-//
-//
+        ValueJoiner<TotalRevenue, Store, TotalRevenueWithAddress> valueJoiner = TotalRevenueWithAddress::new;
+
+
+        Joined<String, TotalRevenue, Store> joinedParams = Joined.with(
+                Serdes.String(),
+                SerdesFactory.totalRevenueSerdes(),
+                SerdesFactory.storeSerdes()
+        );
+
+        revenueKTable
+                .toStream()
+                        .map((key, value) -> KeyValue.pair(key.key(), value))
+                .join(storesTable, valueJoiner, joinedParams)
+                .print(Printed.<String, TotalRevenueWithAddress>toSysOut().withLabel(storedName+"-bystore"));
+
+
+
 //        revenueWithStoreTable
 //                .toStream()
 //                .print(Printed.<String, TotalRevenueWithAddress>toSysOut().withLabel(storedName + "-bystore"));
@@ -223,7 +247,7 @@ public class OrdersTopology {
                 .print(Printed.<String, TotalCountWithAddress>toSysOut().withLabel(storedName + "-bystore"));
     }
 
-    private static void printLocalDateTimes(Windowed<String> key, Long value) {
+    private static void printLocalDateTimes(Windowed<String> key, Object value) {
         var startTime = key.window().startTime();
         var endTime = key.window().endTime();
         log.info("startTime: {} , endTime : {}", startTime, endTime);
